14332212edd94068363676f7b7d1becd8665cd8e
Rahul Sahay <rahulsahay19@hotmail.com>
2020-01-09 00:25:15 +0530

    undone playground

--- src/app/home/home.component.sandbox.ts
+++
@@ -1,7 +0,0 @@
-import { sandboxOf } from 'angular-playground';
-import { HomeComponent } from './home.component';
-
-export default sandboxOf(HomeComponent)
-  .add('Home Component', {
-    template: `<app-home></app-home>`
-  });
